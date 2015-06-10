7a96eb1ff638d5507ec4764d4fd720b3367761df
jam23 <juan.acosta.morillo@gmail.com>
2015-06-09 21:13:32 -0430

    Modulo Gestion de Condiciones de Pagos
    Modulo de Gestion de Condiciones de Pagos terminado.
    Skin para los TextBoxes.

---
+++ SistemaFacturacion/GestionCondicionesPago.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Theme="Estado" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="GestionCondicionesPago.aspx.cs" Inherits="SistemaFacturacion.GestionCondicionesPago" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <%Page.Header.Title = "Gestión de Condiciones de Pagos"; %>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="form-group">
+                <asp:Label ID="Label1" runat="server" Text="Id"></asp:Label>
+                <asp:TextBox ID="txtId" runat="server" ReadOnly="true"></asp:TextBox>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="form-group">
+                <asp:Label ID="Label2" runat="server" Text="Descripción"></asp:Label>
+                <asp:TextBox ID="txtDescripcion" runat="server"></asp:TextBox>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="form-group">
+                <asp:Label ID="Label4" runat="server" Text="Cantidad de Días"></asp:Label>
+                <asp:TextBox ID="txtCantidadDias" MaxLength="3" runat="server"></asp:TextBox>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="form-group">
+                <asp:Label ID="Label3" runat="server" Text="Estado"></asp:Label>
+                <asp:DropDownList runat="server" ID="ddlEstado" SkinID="ddlEstado">
+                </asp:DropDownList>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="form-group">
+                <asp:Button ID="btnCrear" runat="server" Text="Crear" CssClass="btn btn-primary" OnClick="btnCrear_Click" />
+                <asp:Button ID="btnGuardar" runat="server" Text="Guardar" CssClass="btn btn-success" Enabled="false" OnClick="btnGuardar_Click" />
+                <asp:Button ID="btnEliminar" runat="server" Text="Eliminar" CssClass="btn btn-danger" Enabled="false" OnClick="btnEliminar_Click" />
+                <asp:Button ID="btnCancelar" runat="server" Text="Cancelar" CssClass="btn btn-warning" OnClick="btnCancelar_Click" />
+            </div>
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <asp:GridView ID="gvCategorias" runat="server"
+                CssClass="table table-striped table-bordered table-hover dataTable no-footer" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:TemplateField HeaderText="ID">
+                        <ItemTemplate>
+                            <asp:LinkButton ID="lkbId" runat="server" Text='<%# Bind("id") %>' OnClick="lkbId_Click"></asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:BoundField DataField="descripcion" HeaderText="Descripci&#243;n"></asp:BoundField>
+                    <asp:BoundField DataField="cantidadDias" HeaderText="Cantidad de Días"></asp:BoundField>
+                    <asp:BoundField DataField="estado" HeaderText="Estado"></asp:BoundField>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
+</asp:Content>
---
+++ SistemaFacturacion/GestionCondicionesPago.aspx.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemaFacturacion
+{
+    public partial class GestionCondicionesPago : System.Web.UI.Page
+    {
+        FACTURACIONEntities db = new FACTURACIONEntities();
+        private static CRUD operacion = CRUD.Ninguna;
+        SweetAlert message = new SweetAlert(showCancelButton: false);
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ///TODO: Arreglar los nombre de las columnas del grid;
+            cargarGridView();
+        }
+
+        protected void lkbId_Click(object sender, EventArgs e)
+        {
+            ///TODO: EXtraer un metodo para obtener el id del linkbutton
+            operacion = CRUD.Actualizar;
+            btnCrear.Enabled = false;
+            btnGuardar.Enabled = true;
+            btnEliminar.Enabled = true;
+            LinkButton lkb = (LinkButton)sender;
+            int Id = Int32.Parse(lkb.Text);
+            var item  = db.CONDICIONESPAGO.Find(Id);
+
+            txtId.Text = Id.ToString();
+            txtDescripcion.Text = item.descripcion;
+            txtCantidadDias.Text = item.cantidadDias.ToString();
+            ddlEstado.ClearSelection();
+            ddlEstado.Items.FindByValue(item.estado).Selected = true;
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            GuardarCambiosRealizados();
+        }
+
+        private void GuardarCambiosRealizados()
+        {
+
+            if (ValidarCampos())
+            {
+                try
+                {
+                    CONDICIONESPAGO item = new CONDICIONESPAGO();
+
+                    switch (operacion)
+                    {
+                        case CRUD.Crear:
+                            item.descripcion = txtDescripcion.Text;
+                            item.estado = ddlEstado.SelectedValue;
+                            item.cantidadDias = Int32.Parse(txtCantidadDias.Text);
+                            db.CONDICIONESPAGO.Add(item);
+                            break;
+                        case CRUD.Actualizar:
+                            item = db.CONDICIONESPAGO.Find(Int32.Parse(txtId.Text));
+                            item.descripcion = txtDescripcion.Text;
+                            item.cantidadDias = Int32.Parse(txtCantidadDias.Text);
+                            item.estado = ddlEstado.SelectedValue;
+                            db.Entry(item).State = System.Data.EntityState.Modified;
+                            break;
+                        case CRUD.Eliminar:
+                            item = db.CONDICIONESPAGO.Find(Int32.Parse(txtId.Text));
+                            db.CONDICIONESPAGO.Remove(item);
+                            break;
+                        default:
+                            break;
+                    }
+                    db.SaveChanges();
+                    LimpiarCampos();
+                    cargarGridView();
+                    message.title = "Operación Realizada.";
+                    message.type = "success";
+                }
+                catch (Exception)
+                {
+                    message.title = "Ocurrio algún problema, favor de verificar.";
+                    message.type = "error";
+                    throw;
+                }
+                finally
+                {
+                    operacion = CRUD.Ninguna;
+                }
+
+                this.ShowMessage(message);
+            }
+        }
+
+        protected void btnCrear_Click(object sender, EventArgs e)
+        {
+            operacion = CRUD.Crear;
+            btnGuardar.Enabled = true;
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+            operacion = CRUD.Eliminar;
+            GuardarCambiosRealizados();
+            cargarGridView();
+        }
+
+        private void cargarGridView()
+        {            
+            gvCategorias.DataSource = db.CONDICIONESPAGO.ToList();
+            gvCategorias.DataBind();
+        }
+
+        /// <summary>
+        /// Metodo para reiniciar todos los controles a sus estados iniciales.
+        /// </summary>
+        private void LimpiarCampos()
+        {
+            operacion = CRUD.Ninguna;
+            txtId.Text = String.Empty;
+            txtDescripcion.Text = String.Empty;
+            txtCantidadDias.Text = String.Empty;
+            btnGuardar.Enabled = false;
+            btnEliminar.Enabled = false;
+            btnCrear.Enabled = true;
+            ddlEstado.SelectedIndex = 0;
+        }
+
+        private bool ValidarCampos()
+        {
+            int cantDias = 0;
+            if (String.IsNullOrEmpty(txtDescripcion.Text))
+            {
+                message.title = "El campo Descripción es obligatorio.";
+                message.type = "warning";
+                this.ShowMessage(message);
+                return false;
+            }
+            else if(String.IsNullOrEmpty(txtCantidadDias.Text) || !Int32.TryParse(txtCantidadDias.Text,out cantDias))
+            {
+                message.title = "Verificar el Campo Cantidad de Días, datos incorrectos o está vacío.";
+                message.type = "warning";
+                this.ShowMessage(message);
+                return false;
+            }
+
+            return true;
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+    }
+}
